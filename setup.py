81e85d80004bcea48b80a833d29e9f2f6465b511
LukaLukas <luka93.pavlica@outlook.com>
2021-06-16 15:15:20 +0200

    this is first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='dokr',
+      version='0.1',
+      # description='The funniest joke in the world',
+      # url='http://github.com/storborg/funniest',
+      # author='Luka Pavlica',
+      # author_email='luka.pavlica93@gmail.com',
+      # license='MIT',
+      packages=['dokr'
+                ],
+      # zip_safe=False
+)
